7864926f13298a80d49a642a34dba1b203f8f3d6
Tamara <tabualhsan@gmail.com>
2021-05-17 13:50:26 -0700

    created x player event listeners

--- src/index.js
+++ src/index.js
@@ -3,19 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    render() {
+        
+    render(){
       return (
-        <button className="square">
-          {/* TODO */}
+          <button 
+          className="square" 
+          onClick={() => this.props.onClick()}
+          >
+          {this.props.value}
         </button>
       );
     }
-  }
+}
+ 
   
   class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
+
+    constructor(props){
+    super(props);
+    this.state = {
+        squares: Array(9).fill(null),
+
+    };
+}
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] ="X";
+        this.setState({squares: squares});
+        
     }
+    renderSquare(i) {
+      return ( 
+      <Square 
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}
+      />
+    );
+}
+
+
   
     render() {
       const status = 'Next player: X';
@@ -44,6 +70,7 @@ class Square extends React.Component {
   }
   
   class Game extends React.Component {
+    
     render() {
       return (
         <div className="game">
